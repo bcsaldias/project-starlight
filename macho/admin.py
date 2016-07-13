737852f8d2a54bbd1aa1726ed9d201236af03c24
richardskim111 <richardskim111@gmail.com>
2016-07-13 16:35:10 -0400

    Uploaded Macho and MachoDetail

--- macho/admin.py
+++ macho/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Macho
+from .models import Macho, MachoDetail
 
 
+class DetailInline(admin.TabularInline):
+    model = MachoDetail
+    extra = 0
+    can_delete = False
+    readonly_fields = ('mjd','mag','err',)
 
 
-admin.site.register(Macho)
+class MachoAdmin(admin.ModelAdmin):
+    list_display = ['macho_id', 'period', 'true_label']
+    inlines = [DetailInline]
+    readonly_fields = ('macho_id', 'periodicity', 'period', 'true_label',)
+
+
+admin.site.register(Macho, MachoAdmin)
